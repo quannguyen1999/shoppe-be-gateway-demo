c2b5e61cba742b2db21f45d3a953a0a4c5d305de
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-07 08:43:11 +0700

    set rate limit

--- README.md
+++ README.md
@@ -24,4 +24,10 @@ docker run --name active-mq-test --rm -it -p 15672:15672 -p 5672:5672 rabbitmq:3
 # Command run postgres
 docker run --name postgres -p 5431:5432 POSTGRES_PASSWORD=postgres -d postgres
 
+# access link to get info all path 
 >access link for more detail:http://10.0.0.113:8073/actuator/gateway/routes
+
+# access link for diagnosis circuit breaker
+http://10.0.0.85:8073/actuator/circuitbreakers
+# and to get detail 
+http://10.0.0.85:8073/actuator/circuitbreakerevents?name=accountCircuitBreaker
--- src/main/java/com/shopee/ecommer/shopeebegatewaydemo/ShopeeBeGatewayDemoApplication.java
+++ src/main/java/com/shopee/ecommer/shopeebegatewaydemo/ShopeeBeGatewayDemoApplication.java
@@ -1,7 +1,20 @@
 package com.shopee.ecommer.shopeebegatewaydemo;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class ShopeeBeGatewayDemoApplication {
@@ -9,5 +22,49 @@ public class ShopeeBeGatewayDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopeeBeGatewayDemoApplication.class, args);
 	}
+
+
+	@Bean
+	public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/account/**")
+						.filters( f -> f.rewritePath("/account/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+
+								.circuitBreaker(config -> config.setName("accountCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
+
+						.uri("lb://ACCOUNT"))
+				.route(p -> p
+						.path("/admin/**")
+						.filters( f -> f.rewritePath("/admin/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+//								.retry(retryConfig -> retryConfig.setRetries(3)
+//										.setMethods(HttpMethod.GET)
+//										.setBackoff(Duration.ofMillis(1000),Duration.ofMillis(1000),2,true)))
+						.uri("lb://ADMIN"))
+				.route(p -> p
+						.path("/email/**")
+						.filters( f -> f.rewritePath("/email/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+//								.retry(retryConfig -> retryConfig.setRetries(3)
+//										.setMethods(HttpMethod.GET)
+//										.setBackoff(Duration.ofMillis(1000), Duration.ofMillis(1000),2,true))
+
+						)
+
+						.uri("lb://EMAIL"))
+			.build();
+	}
+
+	// set limit time for circuirbreaker
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(20)).build()).build());
+	}
+
 
 }
